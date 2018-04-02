1606161790c548a11865c2b6541aa1b55b21121b
Carlos Saucedo <carlos.gabriel.saucedo@gmail.com>
2018-04-02 13:25:34 -0400

    The First Update
    Updated readme to add required files.
    Changed auth keys to .txt file-based values.
    Improved cleanUpTweet.
    Began developing robot code.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+![Python 2.7](https://img.shields.io/badge/python-2.7-blue.svg)
 # gizoogle-bot
+A Twitter robot that translates tweets automatically.
+[@GizoogleBot](https://www.twitter.com/GizoogleBot)
+
+##Usage
+Whenever a person tweets @GizoogleBot or #GizoogleThis, this python robot translates the tweet and tweets it back to them.
+This robot includes a delay in order to comply with Twitter guidelines, so not all tweets may be translated.
+
+##Setup
+In order to create your own Gizoogle bot, you must have a file named `auth_tokens.txt`, and place your tokens *in order*. This allows my code to accurately get its auth keys.
--- robot.py
+++ robot.py
@@ -8,33 +8,67 @@ from TwitterFollowBot import TwitterBot
 from BeautifulSoup import BeautifulSoup
 import requests
 import json
-URL = 'http://www.gizoogle.net/textilizer.php' #Gizoogle URL
+from time import sleep
+URL = "http://www.gizoogle.net/textilizer.php" #Gizoogle URL
 
 #Twitter API keys
-CONSUMER_KEY = ''
-CONSUMER_KEY_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_KEY_SECRET = ''
+tokenfile = open("auth_tokens.txt")
+tokens = tokenfile.readlines()
+CONSUMER_KEY = tokens[0]
+CONSUMER_KEY_SECRET = tokens[1]
+ACCESS_KEY = tokens[2]
+ACCESS_KEY_SECRET = tokens[3]
+
+#Other text files
+tweetfile = open("tweets_text.txt")
+tweets = tweetfile.readlines()
+usernamefile = open("tweets_usernames.txt")
+usernames = usernamefile.readlines()
+tweetlog= open("tweetlog.txt")
+
+#Global variables
+currentTweet = "null"
+cleanedTweet = "null"
+isEnabled = True
 
 #Instantiates tweepy bot
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_KEY_SECRET)
-api = tweepy.API(auth)
-
-#Search test
-for result in api.search(q="#python"):
-    print result.text
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 """
-Removes Twitter labels from string.
+Resets the tweet list.
 """
-def removeTwitterTags(string):
-    string.replace('@GizoogleBot', '')
-    string.replace('#GizoogleThis','')
+def resetList():
+    tweets = open("tweets_text.txt", "w")
+    usernames = open("tweets_usernames.txt", "w")
 
 """
 Translates given text.
 """
 def translate(text):
-    html = requests.post(URL, data={'translatetext': text}).text
+    html = requests.post(URL, data={"translatetext": text}).text
     return BeautifulSoup(html).textarea.contents[0].strip()
+
+"""
+Cleans up tweet to be able to be read.
+"""
+def cleanUpTweet(text):
+    output = text.encode("ascii", "ignore")
+    output = output.replace("@GizoogleBot", "")
+    output = output.replace("#GizoogleThis", "")
+    output = output.replace("@gizooglebot", "")
+    output = output.replace("#gizooglethis", "")
+    return output
+
+#Actual code
+"""
+while(isEnabled):
+    print("Starting Bot.")
+    for result in api.search(q="@GizoogleBot", count=1):
+        if(currentTweet != result.text):
+            currentTweet = result.text
+            print("tweet author: " + result.author.screen_name)
+            print(translate(cleanUpTweet(currentTweet)))
+        sleep(60)
+"""
